c25075ba638d2e4a787a151ec21286661c5e7499
Сегеда <Alexey.Segeda@x5.ru>
2020-08-25 20:35:12 +0300

    Small fix

--- service/src/main/java/saint/cheshire/service/ExampleService.java
+++ service/src/main/java/saint/cheshire/service/ExampleService.java
@@ -3,6 +3,8 @@ package saint.cheshire.service;
 import lombok.NonNull;
 import org.springframework.stereotype.Service;
 
+import static java.lang.String.format;
+
 @Service
 public class ExampleService {
 
@@ -13,7 +15,7 @@ public class ExampleService {
 
     @NonNull
     public String exampleWithArguments(@NonNull String firstArgument, @NonNull String secondArgument) {
-        return "Calling service with simple example command with args {} and {}";
+        return format("Calling service with simple example command with args %s and %s", firstArgument, secondArgument);
     }
 
 }
